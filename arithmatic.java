eee73286be72eea3ca05b8a4dafa56b7c759d04c
Murukesh1996 <43032365+Murukesh1996@users.noreply.github.com>
2018-09-07 11:46:32 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    int num1=6, num2=2,div,add,sub,mul;
+    div=num1/num2;
+    add=num1+num2;
+    sub=num1-num2;
+    mul=num1*num2;
+    System.out.println(add);
+    System.out.println(div);
+    System.out.println(sub);
+    System.out.println(mul);
+  }
+}
