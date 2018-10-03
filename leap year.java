2a7b53393170a1908387ceaca2fcef5bb9f23946
Murukesh1996 <43032365+Murukesh1996@users.noreply.github.com>
2018-10-03 12:16:05 +0530

    Create leap year.java

---
+++ leap year.java
@@ -0,0 +1,14 @@
+public class main {
+public static void main (String args[]){
+    int leapYear = 1900;
+
+    if (leapYear <= 2020){
+        for (int x = 0; x <= 10; x++){ 
+            System.out.print(leapYear + " ");
+            leapYear = leapYear + 4;
+        }
+        System.out.println(" ");
+        int x = 0;
+    }
+}
+}
