09ab2b2032dbedf58fa14f4ef7120f09cc2af677
Murukesh1996 <43032365+Murukesh1996@users.noreply.github.com>
2018-10-03 12:35:36 +0530

    Create even or odd.java

---
+++ even or odd.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class OddOrEven
+{
+   public static void main(String args[])
+   {
+      int x=101;
+      System.out.println("Enter an integer to check if it is odd or even");
+     // x=in.nextInt();
+      if(x % 2==0)
+         System.out.println("The number is even.");
+      else
+         System.out.println("The number is odd.");
+   }
+}
