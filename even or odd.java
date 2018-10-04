0c2a516eab53c3dc3db345049126a689a9263d23
Murukesh1996 <43032365+Murukesh1996@users.noreply.github.com>
2018-10-04 11:31:00 +0530

    Update even or odd.java

--- even or odd.java
+++ even or odd.java
@@ -1,14 +1,13 @@
-import java.io.*;
-public class OddOrEven
-{
-   public static void main(String args[])
-   {
-      int x=101;
-      System.out.println("Enter an integer to check if it is odd or even");
-     // x=in.nextInt();
-      if(x % 2==0)
-         System.out.println("The number is even.");
-      else
-         System.out.println("The number is odd.");
-   }
+class Main {
+  public static void main(String[] args) {
+    int num;
+      
+         num=10;
+          if(num%2==0)
+            System.out.println(num + "the num is even");
+        else 
+            System.out.println(num + "the num is odd ");
+       
+         
+  }
 }
